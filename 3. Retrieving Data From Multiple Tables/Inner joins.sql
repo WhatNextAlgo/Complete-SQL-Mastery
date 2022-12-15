110e6df320da1c611fb8fcd8788e2c5627442cf6
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-15 12:29:07 +0530

    Update Inner joins.sql

--- 3. Retrieving Data From Multiple Tables/Inner joins.sql
+++ 3. Retrieving Data From Multiple Tables/Inner joins.sql
@@ -4,3 +4,14 @@ USE sql_store;
 SELECT o.order_id, c.customer_id,c.first_name,c.last_name
 FROM orders o 
 INNER JOIN customers c on o.customer_id = c.customer_id
+
+-- Exercise
+"""
+Join the order_items table to products table,
+get the info of particular order what is product id , name, quantity and unit price.
+Unit Price should be when the order is placed.
+"""
+
+SELECT oi.order_id,p.product_id,oi.quantity,oi.unit_price
+FROM order_items oi
+INNER JOIN products p on oi.product_id = p.product_id
