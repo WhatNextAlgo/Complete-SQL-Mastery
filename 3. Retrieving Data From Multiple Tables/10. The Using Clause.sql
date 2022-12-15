bc1a281e55d3dec892634cafc10e71fc3cf0a430
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-15 14:04:58 +0530

    Create 10. The Using Clause.sql

---
+++ 3. Retrieving Data From Multiple Tables/10. The Using Clause.sql
@@ -0,0 +1,33 @@
+SELECT 
+	o.order_id,
+    c.first_name,
+    sh.name AS shipper
+FROM orders o
+JOIN customers c 
+	-- ON o.customer_id = c.customer_id
+    USING (customer_id)
+LEFT JOIN shippers sh 
+	-- ON o.shipper_id = sh.shipper_is
+    USING (shipper_id);
+    
+   
+SELECT 
+	*
+FROM order_items oi
+JOIN order_item_notes oin 
+	USING (order_id,product_id);
+
+
+USE sql_invoicing;
+
+SELECT 
+	p.date,
+    c.name as client,
+    p.amount,
+    pm.name as payment_method
+FROM payments p
+JOIN clients c
+	USING (client_id)
+JOIN payment_methods pm 
+	ON p.payment_method = pm.payment_method_id
+	
