35bb18b5ab46fdbf52f1cffc4788828bce6b1251
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-16 12:16:20 +0530

    Create 3. Inserting Multiple Rows.sql

---
+++ 4. Inserting, Updating, and Deleting Data/3. Inserting Multiple Rows.sql
@@ -0,0 +1,13 @@
+INSERT INTO shippers (name)
+VALUES ('Shipper1'),
+		('Shipper2'),
+		('Shipper3')
+    
+  
+-- Exercise
+-- Insert three rows in the products table
+
+INSERT INTO Products (name,quantity_in_stock,unit_price)
+VALUES ('wine',50,5.13),
+		('whisky',40,6.13),
+		('bear',50,2.13)
