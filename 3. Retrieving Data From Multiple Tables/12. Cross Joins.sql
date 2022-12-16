a977f1f33b383d828514724ea04daba351bec290
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-16 11:37:45 +0530

    Update 12. Cross Joins.sql

--- 3. Retrieving Data From Multiple Tables/12. Cross Joins.sql
+++ 3. Retrieving Data From Multiple Tables/12. Cross Joins.sql
@@ -3,4 +3,28 @@ SELECT
     p.name AS product
 FROM  customers c
 CROSS JOIN products p
-Order By c.first_name
+Order By c.first_name;
+
+SELECT 
+	c.first_name AS customer,
+    p.name AS product
+FROM  customers c, products p
+Order By c.first_name;
+
+--Exercise
+-- Do a cross join between shipper and products
+
+-- using implicits syntax
+SELECT 
+    s.name as Shipper,
+    p.name as Product
+FROM shippers s,products p
+Order by 1;
+
+-- and then using the explicit syntax
+SELECT 
+    s.name as Shipper,
+    p.name as Product
+FROM shippers s
+CROSS JOIN products p
+Order by 1;
