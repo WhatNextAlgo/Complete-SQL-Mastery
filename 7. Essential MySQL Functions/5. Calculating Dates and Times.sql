67f406454ed8297d12d69146bbd67b30123a18c0
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-18 12:46:09 +0530

    Create 5. Calculating Dates and Times.sql

---
+++ 7. Essential MySQL Functions/5. Calculating Dates and Times.sql
@@ -0,0 +1,4 @@
+SELECT DATE_ADD(Now(),INTERVAL 1 DAY), DATE_ADD(Now(),INTERVAL 1 Year), DATE_ADD(Now(),INTERVAL -1 Year);
+ Select date_sub(Now(),INTERVAL 1 DAY),date_sub(Now(),INTERVAL 1 Year);
+ Select datediff('2019-01-05','2019-01,01');
+ Select time_to_sec('09:00') - time_to_sec('09:02')
