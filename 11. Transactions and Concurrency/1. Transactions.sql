d5791c3dda7d1475b5c9b127d0ce46c5042304d5
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-20 11:33:38 +0530

    Create 1. Transactions.sql

---
+++ 11. Transactions and Concurrency/1. Transactions.sql
@@ -0,0 +1,10 @@
+/*
+TRANSACTIONS : A group of SQL statements that represent a single unit of work.
+
+Transactions has ACID properties: 
+  1. Atomicity
+  2. Consistency
+  3. Isolation
+  4. Durability
+*/
+
