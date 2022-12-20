ef413c28644a6b86ea41b5394a0202687264454e
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-20 13:15:41 +0530

    Create 2. String Types.sql

---
+++ 12. Data Types/2. String Types.sql
@@ -0,0 +1,14 @@
+/*
+CHAR(x) fixed-length
+VARCHAR(x) variable-length , max : 65,535 charachter (~ 64KB)
+MEDIUMTEXT max: 16MB
+LONGTEXT max:4GB
+-----------------------------------------------------------------
+TINYTEXT max: 255 bytes
+TEXT max: 64KB
+
+BYTES USE BY DIFFERENT LANGUAGES:
+ENGLISH : 1 bytes
+EUROPEAN MIDDLE-EASTER : 2 bytes
+ASIAN: 3 bytes
+*/
