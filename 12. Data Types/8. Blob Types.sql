b6c3ceb085692007e57b13afb393169566cd8440
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-20 13:47:13 +0530

    Create 8. Blob Types.sql

---
+++ 12. Data Types/8. Blob Types.sql
@@ -0,0 +1,13 @@
+/*
+8. Blob Types
+TINYBLOB 255b
+BLOB 65KB
+MEDIUMBLOB 16MB
+LONGBLOB 4GB
+
+PROBLEM WITH STORING FILES IN A DATABASE
+1. Increased database size
+2. slower backups
+3. performance problems
+4. More code to read/write images
+*/
