0e933c7559e17cebd06bd8fc350d4805ed5f33b1
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-21 11:21:36 +0530

    Create 3. Conceptual Model.sql

---
+++ 13. Design Databases/3. Conceptual Model.sql
@@ -0,0 +1,13 @@
+/*
+  CONCEPTUAL MODEL: Represents the entities and their relationships.
+  
+  Two ways we can represents CONCEPTUAL MODEL:
+  1. Entity Relationship (ER)
+  2. UML Diagrams (Unified Modelling Languages).
+  
+  There are various Modelling Tools:
+    1. Microsoft Visio.
+    2. Draw.io
+    3. LucidCharts
+  
+*/
