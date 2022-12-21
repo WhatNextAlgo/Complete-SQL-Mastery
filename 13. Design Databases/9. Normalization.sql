6493b5e098a86f240f2b352f91e206d29f37ecc0
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-21 12:17:50 +0530

    Create 9. Normalization.sql

---
+++ 13. Design Databases/9. Normalization.sql
@@ -0,0 +1,5 @@
+/*
+NORMALIZATION: Normalization is a process of reviewing our design and ensuring that it follows few predefined rules that prevents data duplication.
+There are 7 rules also called 7 Normal Form.
+
+*/
