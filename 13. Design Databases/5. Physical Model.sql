f7574529b6fbcce5f8e78fad2f0a8dcd15eceaef
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-21 12:13:04 +0530

    Create 5. Physical Model.sql

---
+++ 13. Design Databases/5. Physical Model.sql
@@ -0,0 +1,4 @@
+/*
+PHYSICAL MODEL: Physical model is real implementation of logical model with more details information about entities and relationship like dataType, primaryKey,
+foreignKey, compositekey etc.
+*/
