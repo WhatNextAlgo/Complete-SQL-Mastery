29ea5d8731477356999109b9673f8a8de74a5b3a
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-22 13:14:07 +0530

    Create 12. Indexes Maintenance.sql

---
+++ 14. Indexing for High Performance/12. Indexes Maintenance.sql
@@ -0,0 +1,6 @@
+/*
+
+INDEXES MAINTENANCE : Before creating indexes, check the existing onces to avoids Duplicate and Redundant indexes.
+                      Index maintenance is very important drop the indexed which are in not use , duplicate , and Redundant one.
+   
+*/
