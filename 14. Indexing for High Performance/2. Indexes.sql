e0657e4cc5945c8dbbd33a4041212a2db84dbc6f
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-12-22 11:25:49 +0530

    Create 2. Indexes.sql

---
+++ 14. Indexing for High Performance/2. Indexes.sql
@@ -0,0 +1,11 @@
+/*
+INDEXES : Indexes are use for getting Data quicklyand design indexes based on your queries, not your tables, but it come with a cost
+
+COST OF INDEXES :
+  1. Increase the database 
+  2. slow down te writes
+  
+for this reason, Reserve indexes for performance critical queries.
+  
+
+*/
